76178664e1af614a2b711c389bf09b5990066acd
jordigb4 <jordi.granja.bayot@gmail.com>
2025-05-11 21:10:09 +0200

    Initial controller commit

---
+++ kpi/kpi_controller.py
@@ -0,0 +1,9 @@
+class KPIController:
+    """
+    Controller for KPI usage.
+
+    Daily predictions and states interactions -> Controller -> KPI -> Accessible via Web3
+    """
+
+    def __init__(self):
+        raise NotImplemented
